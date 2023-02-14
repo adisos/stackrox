6c2531dcae93d146796df7d3ab551156ea94f5ce
adisos <adisos@il.ibm.com>
2023-02-15 00:19:07 +0200

    update to v0.1.1 + add unit tests wip
    Signed-off-by: adisos <adisos@il.ibm.com>

--- roxctl/analyze/netpol/netpol.go
+++ roxctl/analyze/netpol/netpol.go
@@ -1,21 +1,28 @@
 package netpol
 
 import (
-	"path/filepath"
+	"os"
 
 	npguard "github.com/np-guard/netpol-analyzer/pkg/netpol/connlist"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
+	"github.com/stackrox/rox/pkg/errox"
 	"github.com/stackrox/rox/roxctl/common/environment"
-	"github.com/stackrox/rox/roxctl/common/printer"
 )
 
 type analyzeNetpolCommand struct {
 	// Properties that are bound to cobra flags.
-	inputFolderPath string
+	stopOnFirstError      bool
+	treatWarningsAsErrors bool
+	inputFolderPath       string
+	outputFilePath        string
+	removeOutputPath      bool
+	outputToFile          bool
+	focusWorkload         string
 
 	// injected or constructed values
-	env     environment.Environment
-	printer printer.ObjectPrinter
+	env environment.Environment
+	//printer printer.ObjectPrinter
 }
 
 // Command defines the netpol command tree
@@ -26,34 +33,51 @@ func Command(cliEnvironment environment.Environment) *cobra.Command {
 		Args: cobra.ExactArgs(1),
 		RunE: func(c *cobra.Command, args []string) error {
 			analyzeNetpolCmd.env.Logger().WarnfLn("This is a Technology Preview feature. Red Hat does not recommend using Technology Preview features in production.")
-			res, err := analyzeNetpolCmd.construct(args, c)
-			analyzeNetpolCmd.printNetpolAnalysis(res)
-			/*synth, err := generateNetpolCmd.construct(args, c)
+			analyzer, err := analyzeNetpolCmd.construct(args, c)
 			if err != nil {
 				return err
 			}
-			if err := generateNetpolCmd.validate(); err != nil {
+			if err := analyzeNetpolCmd.validate(); err != nil {
 				return err
 			}
-			return generateNetpolCmd.generateNetpol(synth)*/
-			return err
+			return analyzeNetpolCmd.analyzeNetpols(analyzer)
 		},
 	}
-	/*c.Flags().BoolVar(&generateNetpolCmd.treatWarningsAsErrors, "strict", false, "treat warnings as errors")
-	c.Flags().BoolVar(&generateNetpolCmd.stopOnFirstError, "fail", false, "fail on the first encountered error")
-	c.Flags().BoolVar(&generateNetpolCmd.removeOutputPath, "remove", false, "remove the output path if it already exists")
-	c.Flags().StringVarP(&generateNetpolCmd.outputFolderPath, "output-dir", "d", "", "save generated policies into target folder - one file per policy")
-	c.Flags().StringVarP(&generateNetpolCmd.outputFilePath, "output-file", "f", "", "save and merge generated policies into a single yaml file")*/
+
+	c.Flags().BoolVar(&analyzeNetpolCmd.treatWarningsAsErrors, "strict", false, "treat warnings as errors")
+	c.Flags().BoolVar(&analyzeNetpolCmd.stopOnFirstError, "fail", false, "fail on the first encountered error")
+	c.Flags().BoolVar(&analyzeNetpolCmd.removeOutputPath, "remove", false, "remove the output path if it already exists")
+	c.Flags().StringVarP(&analyzeNetpolCmd.outputFilePath, "output-file", "f", "", "save connlist output into a txt file")
+	c.Flags().StringVarP(&analyzeNetpolCmd.outputFilePath, "focus-workload", "", "", "focus connections of specified workload name in the output")
 	return c
 }
-func (cmd *analyzeNetpolCommand) construct(args []string, c *cobra.Command) (string, error) {
+func (cmd *analyzeNetpolCommand) construct(args []string, c *cobra.Command) (netpolAnalyzer, error) {
 	cmd.inputFolderPath = args[0]
-	//return npguard.NewPoliciesSynthesizer(opts...), nil
-	conns, err := npguard.FromDir(cmd.inputFolderPath, filepath.WalkDir)
-	if err != nil {
-		return "", err
+	var opts []npguard.ConnlistAnalyzerOption
+	if cmd.env != nil && cmd.env.Logger() != nil {
+		opts = append(opts, npguard.WithLogger(newNpgLogger(cmd.env.Logger())))
 	}
-	res := npguard.ConnectionsListToString(conns)
-	return res, err
+	if cmd.stopOnFirstError {
+		opts = append(opts, npguard.WithStopOnError())
+	}
+	if cmd.focusWorkload != "" {
+		opts = append(opts, npguard.WithFocusWorkload(cmd.focusWorkload))
+	}
+	return npguard.NewConnlistAnalyzer(opts...), nil
+}
 
+func (cmd *analyzeNetpolCommand) validate() error {
+	if err := cmd.setupPath(cmd.outputFilePath); err != nil {
+		return errors.Wrap(err, "failed to set up file path")
+	}
+	return nil
+}
+
+func (cmd *analyzeNetpolCommand) setupPath(path string) error {
+	if _, err := os.Stat(path); err == nil && !cmd.removeOutputPath {
+		return errox.AlreadyExists.Newf("path %s already exists. Use --remove to overwrite or select a different path.", path)
+	} else if !os.IsNotExist(err) {
+		return errors.Wrapf(err, "failed to check if path %s exists", path)
+	}
+	return nil
 }
---
+++ roxctl/analyze/netpol/netpol_test.go
@@ -0,0 +1,92 @@
+package netpol
+
+import (
+	"os"
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stackrox/rox/roxctl/common/mocks"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestAnalyzeNetpolCommand(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, new(analyzeNetpolTestSuite))
+}
+
+type analyzeNetpolTestSuite struct {
+	suite.Suite
+}
+
+func (d *analyzeNetpolTestSuite) TestAnalyzeNetpol() {
+	cases := map[string]struct {
+		inputFolderPath       string
+		expectedAnalysisError error
+		expectedValidateError error
+		strict                bool
+		stopOnFirstErr        bool
+		outFile               string
+		outDir                string
+		removeOutputPath      bool
+	}{
+
+		"not existing inputFolderPath should raise 'os.ErrNotExist' error": {
+			inputFolderPath:       "/tmp/xxx",
+			expectedAnalysisError: os.ErrNotExist,
+		},
+		"happyPath": {
+			inputFolderPath:       "testdata/minimal",
+			expectedAnalysisError: nil,
+		},
+		"treating warnings as errors": {
+			inputFolderPath:       "testdata/empty-yamls",
+			expectedAnalysisError: errNPGWarningsIndicator,
+			strict:                true,
+		},
+	}
+
+	for name, tt := range cases {
+		tt := tt
+		d.Run(name, func() {
+			testCmd := &cobra.Command{Use: "test"}
+			testCmd.Flags().String("output-dir", "", "")
+			testCmd.Flags().String("output-file", "", "")
+
+			env, _, _ := mocks.NewEnvWithConn(nil, d.T())
+			analyzeNetpolCmd := analyzeNetpolCommand{
+				stopOnFirstError:      tt.stopOnFirstErr,
+				treatWarningsAsErrors: tt.strict,
+				inputFolderPath:       "", // set through construct
+				outputFilePath:        tt.outFile,
+				removeOutputPath:      tt.removeOutputPath,
+				env:                   env,
+				//printer:               nil,
+			}
+
+			if tt.outFile != "" {
+				d.Assert().NoError(testCmd.Flags().Set("output-file", tt.outFile))
+			}
+
+			analyzer, err := analyzeNetpolCmd.construct([]string{tt.inputFolderPath}, testCmd)
+			d.Assert().NoError(err)
+
+			err = analyzeNetpolCmd.validate()
+			if tt.expectedValidateError != nil {
+				d.Require().Error(err)
+				d.Assert().ErrorIs(err, tt.expectedValidateError)
+				return
+			}
+			d.Assert().NoError(err)
+
+			err = analyzeNetpolCmd.analyzeNetpols(analyzer)
+			if tt.expectedAnalysisError != nil {
+				d.Require().Error(err)
+				d.Assert().ErrorIs(err, tt.expectedAnalysisError)
+			} else {
+				d.Assert().NoError(err)
+			}
+
+		})
+	}
+
+}
